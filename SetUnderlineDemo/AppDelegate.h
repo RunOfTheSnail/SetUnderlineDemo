c94d825f5943d32ddc26262f060b61639c8f595b
zhangyan <zy@mexue.com>
2017-05-11 20:55:18 +0800

    create

---
+++ SetUnderlineDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SetUnderlineDemo
+//
+//  Created by zhangyan on 2017/5/11.
+//  Copyright © 2017年 zhangyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
